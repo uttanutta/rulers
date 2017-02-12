a38297525e2551ee861f81ddfa6c76962753e9f1
uttanutta <uttanutta@users.noreply.github.com>
2017-02-12 13:58:08 +0000

    Version 2

--- app/src/main/java/uk/gov/boss/rulers/MainActivity.java
+++ app/src/main/java/uk/gov/boss/rulers/MainActivity.java
@@ -2,10 +2,12 @@ package uk.gov.boss.rulers;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
+//
+import android.support.v4.view.MenuItemCompat;
+//
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -33,23 +35,22 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     ListView mainListView;
     ArrayList mNameList = new ArrayList();
     ShareActionProvider mShareActionProvider;
     private static final String PREFS = "prefs";
     private static final String PREF_NAME = "name";
     String jsonRaw;
+
     public class customListItem {
         String title;
         String description;
         String piccie;
     }
     customListItem CustomListItem;
-    customAdapter mArrayAdapter;
+    tomsCustomAdapter mArrayAdapter;
     String selectedView = "england";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,25 +60,20 @@ public class MainActivity extends ActionBarActivity {
         CustomListItem = new customListItem();
         // Access the ListView
         mainListView = (ListView) findViewById(R.id.main_listview);
-        Log.w("point a: ", selectedView);
+        //Log.w("point a: ", selectedView);
         if (savedInstanceState != null) {
             selectedView = savedInstanceState.getString("myView");
             if (selectedView != null) {
                 //default England
-                mArrayAdapter = new customAdapter(selectedView);
+                mArrayAdapter = new tomsCustomAdapter(selectedView);
                 mainListView.setAdapter(mArrayAdapter);
             }
         }
         else {
             //default England
-            mArrayAdapter = new customAdapter("england");
+            mArrayAdapter = new tomsCustomAdapter("england");
             mainListView.setAdapter(mArrayAdapter);
         }
-    }
-
-    public void onSaveInstanceState(Bundle savedState) {
-        super.onSaveInstanceState(savedState);
-        savedState.putString("myView", selectedView);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,13 +98,12 @@ public class MainActivity extends ActionBarActivity {
             // create an Intent with the contents of the TextView
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.setType("text/plain");
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "The Rulers app is quite nice");
-            shareIntent.putExtra(Intent.EXTRA_TEXT, "The Rulers app is reasonably good");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "The Rulers app is quite nice - https://play.google.com/store/apps/details?id=uk.gov.boss.rulers");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "The Rulers app is reasonably good - https://play.google.com/store/apps/details?id=uk.gov.boss.rulers");
             // Make sure the provider knows it should work with that Intent
             mShareActionProvider.setShareIntent(shareIntent);
         }
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -124,6 +119,9 @@ public class MainActivity extends ActionBarActivity {
             case R.id.action_usa:
                 openUsa();
                 return true;
+            case R.id.action_rome:
+                openRome();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -131,49 +129,76 @@ public class MainActivity extends ActionBarActivity {
 
     private void openUsa() {
         selectedView = "usa";
-        mArrayAdapter = new customAdapter(selectedView);
+        mArrayAdapter = new tomsCustomAdapter(selectedView);
         mainListView.setAdapter(mArrayAdapter);
     }
-
     private void openFrance() {
         selectedView = "france";
-        mArrayAdapter = new customAdapter(selectedView);
+        mArrayAdapter = new tomsCustomAdapter(selectedView);
         mainListView.setAdapter(mArrayAdapter);
     }
-
     private void openEngland() {
         selectedView = "england";
-        mArrayAdapter = new customAdapter(selectedView);
+        mArrayAdapter = new tomsCustomAdapter(selectedView);
         mainListView.setAdapter(mArrayAdapter);
     }
-
-    public class customAdapter extends BaseAdapter {
+    private void openRome() {
+        selectedView = "rome";
+        mArrayAdapter = new tomsCustomAdapter(selectedView);
+        mainListView.setAdapter(mArrayAdapter);
+    }
+    public void onSaveInstanceState(Bundle savedState) {
+        //persists view when device is rotated
+        super.onSaveInstanceState(savedState);
+        savedState.putString("myView", selectedView);
+    }
+    public class tomsCustomAdapter extends BaseAdapter {
         //See http://www.codelearn.org/android-tutorial/android-listview
         List<customListItem> customList;
 
-        public customAdapter() {
+        public tomsCustomAdapter() {
             //no parameters, default england
             customList = getDataForListView("england");
         }
 
-        public customAdapter(String filter) {
+        public tomsCustomAdapter(String filter) {
             //with parameters
             customList = getDataForListView(filter);
+        }
+
+        public List<customListItem> getDataForListView(String filter) {
+            //String country
+            List<customListItem> itemsList = new ArrayList<customListItem>();
+            try {
+                JSONObject obj = new JSONObject(jsonRaw);
+
+                JSONArray m_jArray = obj.getJSONArray(filter);
+                for (int i = 0; i < m_jArray.length(); i++) {
+                    JSONObject jo_inside = m_jArray.getJSONObject(i);
+                    customListItem item = new customListItem();
+                    item.title = jo_inside.getString("startYear") + " " + jo_inside.getString("ruler");
+                    item.description = jo_inside.getString("description");
+                    item.piccie = jo_inside.getString("icon");
+                    itemsList.add(item);
+                }
+            }
+            catch(JSONException je){
+                je.printStackTrace();
+            }
+            return itemsList;
         }
 
         @Override
         public int getCount() {
             return customList.size();
         }
-
         @Override
-        public customListItem getItem(int arg0) {
-            return customList.get(arg0);
+        public Object getItem(int position) {
+            return customList.get(position);
         }
-
         @Override
-        public long getItemId(int arg0) {
-            return arg0;
+        public long getItemId(int position) {
+            return position;
         }
 
         @Override
@@ -197,10 +222,6 @@ public class MainActivity extends ActionBarActivity {
                     break;
                 case "bloodycrown":
                     id = getResources().getIdentifier("bloodycrown", "drawable", getPackageName());
-                    itemIcon.setImageResource(id);
-                    break;
-                case "Emperor":
-                    id = getResources().getIdentifier("laurels", "drawable", getPackageName());
                     itemIcon.setImageResource(id);
                     break;
                 case "puritan":
@@ -239,41 +260,36 @@ public class MainActivity extends ActionBarActivity {
                     id = getResources().getIdentifier("frenchpresident", "drawable", getPackageName());
                     itemIcon.setImageResource(id);
                     break;
+                case "jester":
+                    id = getResources().getIdentifier("jester", "drawable", getPackageName());
+                    itemIcon.setImageResource(id);
+                    break;
+                case "Emperor":
+                    id = getResources().getIdentifier("laurels", "drawable", getPackageName());
+                    itemIcon.setImageResource(id);
+                    break;
+                case "Emperor_blood":
+                    id = getResources().getIdentifier("laurelsblood", "drawable", getPackageName());
+                    itemIcon.setImageResource(id);
+                    break;
+                case "Shared_Empire":
+                    id = getResources().getIdentifier("laurels2", "drawable", getPackageName());
+                    itemIcon.setImageResource(id);
+                    break;
+                case "Shared_Empire_blood":
+                    id = getResources().getIdentifier("laurels2blood", "drawable", getPackageName());
+                    itemIcon.setImageResource(id);
+                    break;
                 default:
                     id = getResources().getIdentifier("crown", "drawable", getPackageName());
                     itemIcon.setImageResource(id);
             }
             return arg1;
         }
-
         public customListItem getThing(int position) {
             return customList.get(position);
         }
-
-
-        public List<customListItem> getDataForListView(String filter) {
-            //String country
-            List<customListItem> itemsList = new ArrayList<customListItem>();
-            try {
-                JSONObject obj = new JSONObject(jsonRaw);
-
-                JSONArray m_jArray = obj.getJSONArray(filter);
-                for (int i = 0; i < m_jArray.length(); i++) {
-                    JSONObject jo_inside = m_jArray.getJSONObject(i);
-                    customListItem item = new customListItem();
-                    item.title = jo_inside.getString("startYear") + " " + jo_inside.getString("ruler");
-                    item.description = jo_inside.getString("description");
-                    item.piccie = jo_inside.getString("icon");
-                    itemsList.add(item);
-                }
-            }
-            catch(JSONException je){
-                je.printStackTrace();
-            }
-            return itemsList;
-        }
     }
-
     public String readJsonRulers() {
         InputStream is = this.getResources().openRawResource(R.raw.rulers);
         Writer writer = new StringWriter();
@@ -293,7 +309,7 @@ public class MainActivity extends ActionBarActivity {
             closeQuietly(is);
         }
         String jsonString = writer.toString();
-        //Log.w("jsonString: ", jsonString);
+        Log.w("jsonString: ", jsonString);
         return jsonString;
     }
     protected void closeQuietly( InputStream is ) {
@@ -306,3 +322,5 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 }
+
+
